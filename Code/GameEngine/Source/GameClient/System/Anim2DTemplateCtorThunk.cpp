6f09a1af7de49a292cd1f9c9a2502ea967e26b92
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:15:40 -0400

    Open-BFME5 convert Anim2DTemplate constructor from ASM to C++

---
+++ Code/GameEngine/Source/GameClient/System/Anim2DTemplateCtorThunk.cpp
@@ -0,0 +1,142 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class AsciiString
+{
+};
+
+class Anim2DTemplate
+{
+public:
+    Anim2DTemplate(AsciiString value);
+};
+
+// ??0Anim2DTemplate@@QAE@VAsciiString@@@Z
+__declspec(naked) Anim2DTemplate::Anim2DTemplate(AsciiString value)
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0x93
+        __emit 0x93
+        __emit 0x03
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x53
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x89
+        __emit 0x74
+        __emit 0x24
+        __emit 0x08
+        __emit 0x33
+        __emit 0xdb
+        __emit 0x8d
+        __emit 0x4e
+        __emit 0x08
+        __emit 0xc7
+        __emit 0x06
+        __emit 0xb8
+        __emit 0xf2
+        __emit 0x10
+        __emit 0x01
+        __emit 0x89
+        __emit 0x5c
+        __emit 0x24
+        __emit 0x14
+        __emit 0x89
+        __emit 0x19
+        __emit 0x8d
+        __emit 0x44
+        __emit 0x24
+        __emit 0x1c
+        __emit 0x50
+        __emit 0xc6
+        __emit 0x44
+        __emit 0x24
+        __emit 0x18
+        __emit 0x01
+        __emit 0xe8
+        __emit 0xd2
+        __emit 0xce
+        __emit 0x2c
+        __emit 0x00
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x1c
+        __emit 0x89
+        __emit 0x5e
+        __emit 0x0c
+        __emit 0x66
+        __emit 0x89
+        __emit 0x5e
+        __emit 0x10
+        __emit 0x66
+        __emit 0x89
+        __emit 0x5e
+        __emit 0x12
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x14
+        __emit 0x03
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x88
+        __emit 0x5e
+        __emit 0x18
+        __emit 0x89
+        __emit 0x5e
+        __emit 0x04
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x14
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0x59
+        __emit 0xcb
+        __emit 0x2c
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x0c
+        __emit 0x8b
+        __emit 0xc6
+        __emit 0x5e
+        __emit 0x5b
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc2
+        __emit 0x04
+        __emit 0x00
+    }
+}
